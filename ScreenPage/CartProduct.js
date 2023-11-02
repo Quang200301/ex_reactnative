9cecb0923fb9460a4fadee893181f361a29c52cf
Ho Xuan ty <hoxuanty2003qng@gmail.com>
2023-11-02 07:49:10 +0700

    câp nhat code

--- ScreenPage/CartProduct.js
+++ ScreenPage/CartProduct.js
@@ -1,9 +1,9 @@
 import { View, Text, SafeAreaView, FlatList, StyleSheet, Pressable } from 'react-native';
 import React from 'react';
-import { removeItem} from '../Component/features.js/counterSlice';
-import { incrementQuantity} from '../Component/features.js/counterSlice';
-import { decrementQuantity} from '../Component/features.js/counterSlice';
-import { selectTotalAllQuantity,selectTotalAllPrice } from '../Component/features.js/cartSelectors';
+import { removeItem } from '../Component/features.js/counterSlice';
+import { incrementQuantity } from '../Component/features.js/counterSlice';
+import { decrementQuantity } from '../Component/features.js/counterSlice';
+import { selectTotalAllQuantity, selectTotalAllPrice } from '../Component/features.js/cartSelectors';
 import { useSelector, useDispatch } from 'react-redux';
 
 
@@ -15,20 +15,61 @@ export default function CartProduct() {
   const removeItemFromCart = (item) => {
     dispatch(removeItem(item));
   };
-  const incrementItemQuantity = (item)=>{
+  const incrementItemQuantity = (item) => {
     dispatch(incrementQuantity(item));
   };
-  const decrementItemQuantity = (item)=>{
+  const decrementItemQuantity = (item) => {
     dispatch(decrementQuantity(item));
   };
+      const TotalPayment = () => (
+        <View style={styles.totalPayment}>
+            <View style={styles.detailInvoid}>
+                <Text style={styles.TextInvoice}> Sub - Total</Text>
+                <Text style={styles.TextInvoice}>100 $</Text>
+            </View>
+
+            <View style={styles.detailInvoid}>
+                <Text style={styles.TextInvoice}> Delivery Charge</Text>
+                <Text style={styles.TextInvoice}> 10 $</Text>
+            </View>
+
+            <View style={styles.detailInvoid}>
+                <Text style={styles.TextInvoice}> Discount</Text>
+                <Text style={styles.TextInvoice}> 20 $</Text>
+            </View>
+            <View style={[styles.detailInvoid, { fontSize: 17, marginTop: 15 }]}>
+                <Text style={[styles.TextInvoice, { fontSize: 18 }]}> Total</Text>
+                <Text style={[styles.TextInvoice, { fontSize: 18 }]}> {totaPrice()}$</Text>
+            </View>
+            <View style={{ width: '100%', height: "40%" }}>
+                <Button
+                    title="Place my Order"
+                    containerStyle={{
+                        width: "93%",
+                        height: "70%",
+                        alignSelf: 'center',
+                        elevation: 4,
+                        marginTop: 50,
+                    }}
+                    buttonStyle={{
+                        height: '100%',
+                        backgroundColor: '#FEFEFF',
+                        borderRadius: 15,
+
+                    }}
+                    titleStyle={{ color: '#6B50F6', fontSize: 14 }}
+                />
+            </View>
+        </View>
+    )
   const renderItem = ({ item }) => {
     return (
       <SafeAreaView style={styles.container}>
         <View style={styles.showCart}>
           <Text>{item.name}</Text>
-          <Text>{item.totalPrice } $</Text>
+          <Text>{item.totalPrice} $</Text>
           <View style={{ flexDirection: 'row', gap: 14 }}>
-          <Pressable onPress={() => decrementItemQuantity(item)}>
+            <Pressable onPress={() => decrementItemQuantity(item)}>
               <Text style={styles.remove}>-</Text>
             </Pressable>
             <Text>{item.quantity}</Text>
@@ -53,15 +94,15 @@ export default function CartProduct() {
         renderItem={renderItem}
       />
       <View style={styles.totalItem}>
-        <View style={{flexDirection:'row',gap:100}}>
-        <Text style={{fontSize:20,fontWeight:'800'}}>TotalPrice:</Text>
-        <Text style={{fontSize:20,fontWeight:'800'}}>{totalPrice}$</Text>
+        <View style={{ flexDirection: 'row', gap: 100 }}>
+          <Text style={{ fontSize: 20, fontWeight: '800' }}>TotalPrice:</Text>
+          <Text style={{ fontSize: 20, fontWeight: '800' }}>{totalPrice}$</Text>
         </View>
-       <View  style={{flexDirection:'row',gap:110}}>
-       <Text style={{fontSize:20,fontWeight:'800'}}>TotalQuantity:</Text>
-       <Text style={{fontSize:20,fontWeight:'800'}}>{totalQuantity}</Text>
-       </View>
-       
+        <View style={{ flexDirection: 'row', gap: 110 }}>
+          <Text style={{ fontSize: 20, fontWeight: '800' }}>TotalQuantity:</Text>
+          <Text style={{ fontSize: 20, fontWeight: '800' }}>{totalQuantity}</Text>
+        </View>
+
       </View>
     </View>
   )
@@ -100,15 +141,16 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     padding: 10
   },
-  totalItem:{
-    backgroundColor:'#FF9E80',
-    position:"absolute",
-    width:'90%',
-    height:90,
-    justifyContent:"center",
-    alignItems:'center',
-    marginTop:530,
-    marginLeft:20,
-    borderRadius:26
-  }
+  totalItem: {
+    backgroundColor: '#FF9E80',
+    position: "absolute",
+    width: '90%',
+    height: 90,
+    justifyContent: "center",
+    alignItems: 'center',
+    marginTop: 530,
+    marginLeft: 20,
+    borderRadius: 26
+  },
+ 
 })
--- ScreenPage/cartReduxtoolkit.js
+++ ScreenPage/cartReduxtoolkit.js
@@ -1,73 +1,73 @@
-import {View,Text, SafeAreaView, FlatList,StyleSheet, Pressable,Alert} from 'react-native';
+import { View, Text, SafeAreaView, FlatList, StyleSheet, Pressable, Alert } from 'react-native';
 import dataProduct from '../dataProduct';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { AntDesign } from '@expo/vector-icons';
-import { addItem, decrement,increment,removeItem } from '../Component/features.js/counterSlice';
-const cart=({navigation})=>{
-    const dispatch =useDispatch();
- 
-    const addedItems = useSelector((state)=>state.cart.cart);
-    console.log('âddd=>',addedItems)
-    const addToCart  = item=>{
+import { addItem, decrement, increment, removeItem } from '../Component/features.js/counterSlice';
+const cart = ({ navigation }) => {
+    const dispatch = useDispatch();
+
+    const addedItems = useSelector((state) => state.cart.cart);
+    // console.log('âddd=>', addedItems)
+    const addToCart = item => {
         dispatch(addItem(item));
     };
-    const renderItem=({item})=>{
-        return(
+    const renderItem = ({ item }) => {
+        return (
             <SafeAreaView style={styles.containers}>
                 <View style={styles.container}>
                     <Text>{item.name}</Text>
                     <Text>{item.price}</Text>
-                    <Pressable onPress={()=>addToCart (item)}>
-                        <Text style={{borderWidth:1,borderRadius:6}}>Add cart</Text>
+                    <Pressable onPress={() => addToCart(item)}>
+                        <Text style={{ borderWidth: 1, borderRadius: 6 }}>Add cart</Text>
                     </Pressable>
                 </View>
-               
+
             </SafeAreaView>
         )
     }
 
-      return(
-            <SafeAreaView>
-                <View style={styles.header}>
+    return (
+        <SafeAreaView>
+            <View style={styles.header}>
                 <Text>Alo</Text>
-                <Text onPress={()=>navigation.navigate('cartproduct')}>
+                <Text onPress={() => navigation.navigate('cartproduct')}>
                     <AntDesign name="shoppingcart" size={24} color="black" />
                     {/* <Text>{addedItems.cart.cart.length}</Text> */}
                     <Text>{addedItems.length}</Text>
 
                 </Text>
-                
-                </View>
-           
+
+            </View>
+
             <FlatList
-            data={dataProduct}
-            renderItem={renderItem}
-          
+                data={dataProduct}
+                renderItem={renderItem}
+
             />
-            </SafeAreaView>
-      )
-         
-    
+        </SafeAreaView>
+    )
+
+
 }
 export default cart;
 
-const styles =StyleSheet.create({
-    containers:{
-        flex:1,
+const styles = StyleSheet.create({
+    containers: {
+        flex: 1,
 
     },
-    container:{
-       marginHorizontal:32,
-       marginVertical:18,
-       backgroundColor:'#D81B60',
-       margin:12,
-       borderRadius:12,
-       alignItems:'center'
-      
+    container: {
+        marginHorizontal: 32,
+        marginVertical: 18,
+        backgroundColor: '#D81B60',
+        margin: 12,
+        borderRadius: 12,
+        alignItems: 'center'
+
     },
-    header:{
-        flexDirection:'row',
-        justifyContent:'space-around'
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-around'
     }
 })
